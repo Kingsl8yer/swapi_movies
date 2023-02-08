7f0a595ead5166fd88839e7a74f33544858940ef
mike_varona <migueluk91@gmail.com>
2023-02-08 13:25:26 +0000

    Sending a POST Request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect, useCallback} from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from "./components/AddMovie";
 
 function App() {
     const [movies, setMovies] = useState([]);
@@ -13,7 +14,7 @@ function App() {
         setError(null); // reset error state
 
         try {
-            const response = await fetch('https://swapi.dev/api/films/')
+            const response = await fetch('https://swapi-movies-b0158-default-rtdb.firebaseio.com/movies.json');
 
             if (!response.ok) {
                 throw new Error('Something went wrong!');
@@ -21,15 +22,18 @@ function App() {
 
             const data = await response.json();
 
-            const transformedMovies = data.results.map((movieData) => {
-                return {
-                    id: movieData.episode_id,
-                    title: movieData.title,
-                    openingText: movieData.opening_crawl,
-                    releaseDate: movieData.release_date,
-                };
-            });
-            setMovies(transformedMovies);
+            const loadedMovies = [];
+            for(const key in data) {
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate,
+                });
+            }
+
+
+            setMovies(loadedMovies);
             setIsLoading(false);
         } catch (error) {
             setError(error.message);
@@ -39,21 +43,36 @@ function App() {
 
     useEffect(() => {
         fetchMoviesHandler();
-    } , [fetchMoviesHandler]);
+    }, [fetchMoviesHandler]);
+
+    async function addMovieHandler(movie) {
+        const response = await fetch('https://swapi-movies-b0158-default-rtdb.firebaseio.com/movies.json', {
+            method: 'POST',
+            body: JSON.stringify(movie),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+    }
 
     let content = <p>Found no movies.</p>;
-    if(movies.length > 0) {
+    if (movies.length > 0) {
         content = <MoviesList movies={movies}/>;
     }
-    if(error) {
+    if (error) {
         content = <p>{error}</p>;
     }
-    if(isLoading) {
+    if (isLoading) {
         content = <p>Loading...</p>;
     }
 
     return (
         <React.Fragment>
+            <section>
+                <AddMovie onAddMovie={addMovieHandler}/>
+            </section>
             <section>
                 <button onClick={fetchMoviesHandler}>Fetch Movies</button>
             </section>
